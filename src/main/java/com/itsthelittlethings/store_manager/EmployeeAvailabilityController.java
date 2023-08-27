0040454d7750443786fec8bf9ab3202ddb8a28b3
Kendyl Hull <hullkendyl@gmail.com>
2023-08-27 12:09:55 -0400

    Added test - thenInsertEmployeeAvailabilityReturns200OkStatus

---
+++ src/main/java/com/itsthelittlethings/store_manager/EmployeeAvailabilityController.java
@@ -0,0 +1,10 @@
+package com.itsthelittlethings.store_manager;
+
+import org.springframework.http.ResponseEntity;
+
+public class EmployeeAvailabilityController {
+
+    public ResponseEntity<String> insertEmployeeAvailability() {
+        return ResponseEntity.ok().body("");
+    }
+}
---
+++ src/test/java/controller/EmployeeAvailabilityTest.java
@@ -0,0 +1,32 @@
+package controller;
+
+import com.itsthelittlethings.store_manager.EmployeeAvailabilityController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class EmployeeAvailabilityTest {
+
+    @Nested
+    @DisplayName("Given an employee wants to share their availability")
+    class GivenAnEmployeeWantsToShareTheirAvailabilityTest {
+
+        @Nested
+        @DisplayName("When an employee enters their availability for the first time")
+        class WhenAnEmployeeEntersTheirAvailabilityForTheFirstTime {
+
+            @Test
+            @DisplayName("Then insertEmployeeAvailability returns 200 OK status")
+            void thenInsertEmployeeAvailabilityReturns200OkStatus() {
+                final var employeeAvailabilityController = new EmployeeAvailabilityController();
+
+                final var result = employeeAvailabilityController.insertEmployeeAvailability();
+
+                Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+            }
+        }
+    }
+}
