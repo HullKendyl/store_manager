46067a71983a6ab53de6dc56c91c9bff64c548d5
Kendyl Hull <hullkendyl@gmail.com>
2023-08-27 10:13:01 -0400

    Initial Commit - Created base application

---
+++ src/test/java/com/itsthelittlethings/store_manager/StoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsthelittlethings.store_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
