315da2e378a407dcf2c5f3843f0cc783abc146c1
Kendyl Hull <hullkendyl@gmail.com>
2023-08-27 21:12:28 -0400

    Refactored EmployeeAvailabilityServiceTest

--- src/test/java/com/itsthelittlethings/store_manager/service/EmployeeAvailabilityServiceTest.java
+++ src/test/java/com/itsthelittlethings/store_manager/service/EmployeeAvailabilityServiceTest.java
@@ -3,10 +3,7 @@ package com.itsthelittlethings.store_manager.service;
 import com.itsthelittlethings.store_manager.EmployeeAvailability;
 import com.itsthelittlethings.store_manager.EmployeeAvailabilityRepository;
 import com.itsthelittlethings.store_manager.UnableToSaveEmployeeAvailabilityException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -18,9 +15,18 @@ public class EmployeeAvailabilityServiceTest {
     @Mock
     private EmployeeAvailabilityRepository employeeAvailabilityRepository;
 
+    private EmployeeAvailabilityService employeeAvailabilityService;
+    private EmployeeAvailability employeeAvailability;
+
     @Nested
     @DisplayName("Given employee availability is submitted")
     class GivenEmployeeAvailabilityIsSubmittedTest {
+
+        @BeforeEach
+        void setUp() {
+            employeeAvailabilityService = new EmployeeAvailabilityService(employeeAvailabilityRepository);
+            employeeAvailability = new EmployeeAvailability();
+        }
 
         @Nested
         @DisplayName("When the availability is saved to the database")
@@ -29,11 +35,7 @@ public class EmployeeAvailabilityServiceTest {
             @Test
             @DisplayName("Then saveAvailability returns a success message")
             void thenSaveAvailabilityReturnsSuccessMessage() {
-                final var employeeAvailabilityService = new EmployeeAvailabilityService(employeeAvailabilityRepository);
-                final var employeeAvailability = new EmployeeAvailability();
-
                 final var result = employeeAvailabilityService.saveAvailability(employeeAvailability);
-
                 Assertions.assertEquals("Successfully saved employee availability", result);
             }
         }
@@ -45,11 +47,7 @@ public class EmployeeAvailabilityServiceTest {
             @Test
             @DisplayName("Then UnableToSaveEmployeeAvailabilityException is thrown")
             void thenUnableToSaveEmployeeAvailabilityExceptionIsThrown() {
-                final var employeeAvailabilityService = new EmployeeAvailabilityService(employeeAvailabilityRepository);
-                final var employeeAvailability = new EmployeeAvailability();
-
                 Mockito.doThrow(UnableToSaveEmployeeAvailabilityException.class).when(employeeAvailabilityRepository).save(employeeAvailability);
-
                 Assertions.assertThrows(UnableToSaveEmployeeAvailabilityException.class, () ->
                                 employeeAvailabilityService.saveAvailability(employeeAvailability)
                 );
